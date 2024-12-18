b946e6b52f995713728b9d70e8236989f452334d
Suleyman Farajli <suleyman@farajli.net>
2024-12-18 18:25:09 +0400

    touch comment changed

--- src/touch.s
+++ src/touch.s
@@ -4,7 +4,7 @@ _start:
 	mov rdi,[rsp+8+8] 	; get argv[1]
 	; touch sys-call
 	mov rax, 85
-	mov rsi, 438 		; set mode for directory (511 corresponds to 0666 in octal)
+	mov rsi, 438 		; set mode for directory (438 corresponds to 0666 in octal)
 	syscall
 
 exit:
